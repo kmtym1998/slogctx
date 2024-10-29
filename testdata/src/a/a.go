29f0574411f3dca46390514e81c0241eceea3e73
kmtym1998 <kmtym.devloper@gmail.com>
2024-10-29 09:49:31 +0900

    testdata で slog を使う

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,14 @@
 package a
 
-func f() {
+import (
+	"fmt"
+	"log/slog"
+)
+
+func F() {
 	// The pattern can be written in regular expression.
 	var gopher int // want "pattern"
 	print(gopher)  // want "identifier is gopher"
+	fmt.Println(gopher)
+	slog.Info("Hello, World!")
 }
